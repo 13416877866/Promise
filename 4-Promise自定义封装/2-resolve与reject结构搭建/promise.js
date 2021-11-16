c54753e6eb04576b8f1f62a724be47802a06c907
13416877866 <2638706597@qq.com>
2021-11-17 06:54:30 +0800

    27

--- 4-Promise自定义封装/2-resolve与reject结构搭建/promise.js
+++ 4-Promise自定义封装/2-resolve与reject结构搭建/promise.js
@@ -1,12 +1,25 @@
 // 声明构造函数
 function Promise(executor){
+    // 添加属性
+    this.PromiseState="pending";
+    this.PromiseResult=null;
+    // 保存实例对象的this值
+    const self=this;
     // resolve函数
     function resolve(data){
-
+        console.log(this);
+    // 1.修改对象的状态（promiseState)
+    self.PromiseState="fulfilled";//resolved
+    // 2.设置对象结构值（promiseResult)
+    self.PromiseResult=data;
     };
     // reject函数
     function reject(data){
-
+        console.log(this);
+        // 1.修改对象的状态（promiseState)
+        self.PromiseState="rejected";//resolved
+        // 2.设置对象结构值（promiseResult)
+        self.PromiseResult=data;
     };
 // 同步调用[执行器函数]
 executor(resolve,reject);
