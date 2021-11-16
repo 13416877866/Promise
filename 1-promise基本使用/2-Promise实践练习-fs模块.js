097ffc911ef5e2d2edd757fc0eb2f36793f770d6
13416877866 <2638706597@qq.com>
2021-11-16 14:56:56 +0800

    P7 Promise封装fs读取文件操作

--- 1-promise基本使用/2-Promise实践练习-fs模块.js
+++ 1-promise基本使用/2-Promise实践练习-fs模块.js
@@ -1,9 +1,9 @@
 // fs作用对计算机硬盘进行读写操作
 // 这次的案例要求读取resource文件夹下的content.txt的内容
 
-const { rejects } = require('assert');
+
 const fs=require('fs');
-const { resolve } = require('path/posix');
+
 
 
 // 回调函数的形式
@@ -17,7 +17,7 @@ const { resolve } = require('path/posix');
 
 // Promise形式
 let p=new Promise((resolve,reject)=>{
-    fs.readFile('./resource/content.tx',(err,data)=>{
+    fs.readFile('./resource/content.txt',(err,data)=>{
         // 如果出错
         if(err)   reject(err);
         // 如果成功
