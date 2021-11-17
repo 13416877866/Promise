0994fb7f5e7415f72d3c188057b36de8817c75b0
13416877866 <2638706597@qq.com>
2021-11-18 06:15:50 +0800

    42

--- 4-Promise自定义封装/catch 方法与异常穿透/promise.js
+++ 4-Promise自定义封装/catch 方法与异常穿透/promise.js
@@ -1,6 +1,7 @@
-//声明构造函数
-function Promise(executor){
-    //添加属性
+class Promise{
+    // 构造方法
+    constructor(executor){
+          //添加属性
     this.PromiseState = 'pending';
     this.PromiseResult = null;
     //声明属性
@@ -50,123 +51,133 @@ function Promise(executor){
         //修改 promise 对象状态为『失败』
         reject(e);
     }
-}
+    }
+    // then方法
+    then(onResolved,onRejected){
+        const self=this;
+        // 判断回调函数参数
+        if(typeof onRejected!=='function'){
+            onRejected=reason=>{
+                throw reason;
+            };
+        }
+        if(typeof onResolved!=='function'){
+            onResolved=value=>value;
+            // value=>{return value};
+        }
+        return new Promise((resolve,reject)=>{
+        // 封装函数
+        function callback(type){
+            try{
+                // 获取回调函数的执行结果
+           let result=type(self.PromiseResult);
+           // 判断
+           if(result instanceof Promise){
+               result.then(v=>{
+                   resolve(v);
+               },r=>{
+                   reject(r);
+               });
+           }else{
+               // 结果的对象状态为成功
+               resolve(result);
+           }
+           }catch(e){
+               reject(e);
+           }
+        }
+        //调用回调函数  PromiseState
+        if(this.PromiseState === 'fulfilled'){
+            setTimeout(()=>{
+                callback(onResolved);
+            });
+        }
+        if(this.PromiseState === 'rejected'){
+            setTimeout(()=>{
+                callback(onRejected);
+            });
+        }
+        //判断 pending 状态
+        if(this.PromiseState==='pending'){
+            // 保存回调函数
+            this.callbacks.push({
+                onResolved:function(){
+                  callback(onResolved);
+                },
+                onRejected:function(){
+                   callback(onRejected);
+                }
+            });
+        }
+    })
+    }
 
-//添加 then 方法
-Promise.prototype.then = function(onResolved, onRejected){
-    const self=this;
-    // 判断回调函数参数
-    if(typeof onRejected!=='function'){
-        onRejected=reason=>{
-            throw reason;
-        };
+    // catch方法
+    catch(onRejected){
+        return this.then(undefined, onRejected);
     }
-    if(typeof onResolved!=='function'){
-        onResolved=value=>value;
-        // value=>{return value};
-    }
-    return new Promise((resolve,reject)=>{
-    // 封装函数
-    function callback(type){
-        try{
-            // 获取回调函数的执行结果
-       let result=type(self.PromiseResult);
-       // 判断
-       if(result instanceof Promise){
-           result.then(v=>{
-               resolve(v);
-           },r=>{
-               reject(r);
-           });
-       }else{
-           // 结果的对象状态为成功
-           resolve(result);
-       }
-       }catch(e){
-           reject(e);
-       }
-    }
-    //调用回调函数  PromiseState
-    if(this.PromiseState === 'fulfilled'){
-        setTimeout(()=>{
-            callback(onResolved);
-        });
-    }
-    if(this.PromiseState === 'rejected'){
-        setTimeout(()=>{
-            callback(onRejected);
-        });
-    }
-    //判断 pending 状态
-    if(this.PromiseState==='pending'){
-        // 保存回调函数
-        this.callbacks.push({
-            onResolved:function(){
-              callback(onResolved);
-            },
-            onRejected:function(){
-               callback(onRejected);
+
+    // 添加resolve方法(不属于实例对象，属于类要加static)
+    static resolve(value){
+        // 返回Promise对象
+        return new Promise((resolve,reject)=>{
+            if(value instanceof Promise){
+                value.then(v=>{
+                    resolve(v);
+                },r=>{
+                    reject(r);
+                })
+            }else{
+                resolve(value);
             }
         });
     }
-})
-}
-//添加 catch 方法
-Promise.prototype.catch = function(onRejected){
-    return this.then(undefined, onRejected);
+
+    // reject方法
+     static reject(reason){
+        return new Promise((resolve,reject)=>{
+            reject(reason);
+        })
+     }
+
+    // all方法
+    static all(promises){
+        // 返回的结果为promise对象
+        return new Promise((resolve,reject)=>{
+            let count=0;
+            let arr=[];
+            for(let i=0;i<promises.length;i++){
+                promises[i].then(v=>{
+                    // 得知对象的状态是成功
+                    // 每个对象都成功
+                    count++;
+                    arr[i]=v;
+                    if(count===promises.length){
+                        resolve(arr);
+                    }
+                },r=>{
+                    reject(r);
+                })
+            }
+        })
+    } 
+
+    // race方法
+    static race(promises){
+        return new Promise((resolve,reject)=>{
+            for(let i=0;i<promises.length;i++){
+               promises[i].then(v=>{
+                   resolve(v);
+               },r=>{
+                   reject(r);
+               })
+            }
+        })
+    }
 }
 
-// 添加resolve方法（属于Promise这个函数对象，不属于实例对象）
-Promise.resolve=function(value){
-    // 返回Promise对象
-    return new Promise((resolve,reject)=>{
-        if(value instanceof Promise){
-            value.then(v=>{
-                resolve(v);
-            },r=>{
-                reject(r);
-            })
-        }else{
-            resolve(value);
-        }
-    });
-}
-// 添加reject方法
-Promise.reject=function(reason){
-   return new Promise((resolve,reject)=>{
-       reject(reason);
-   })
-}
-// 添加all方法
-Promise.all=function(promises){
-    // 返回的结果为promise对象
-    return new Promise((resolve,reject)=>{
-        let count=0;
-        let arr=[];
-        for(let i=0;i<promises.length;i++){
-            promises[i].then(v=>{
-                // 得知对象的状态是成功
-                // 每个对象都成功
-                count++;
-                arr[i]=v;
-                if(count===promises.length){
-                    resolve(arr);
-                }
-            },r=>{
-                reject(r);
-            })
-        }
-    })
-}
-// 添加race方法
-Promise.race=function(promises){
-    return new Promise((resolve,reject)=>{
-        for(let i=0;i<promises.length;i++){
-           promises[i].then(v=>{
-               resolve(v);
-           },r=>{
-               reject(r);
-           })
-        }
-    })
-}
+
+
+
+
+
